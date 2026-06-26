0ce26ab3a763184af561a0565663a12f3d7b9454
vibinsgit <vibinsv810@gmail.com>
2026-06-26 23:43:04 +0530

    Core1 File commited.

---
+++ Core_Java/Core1.java
@@ -0,0 +1,25 @@
+// Topic : Variable decleration , data type, basic structure
+//         Literals
+
+// Time : 1 : 00 : 00
+
+public class Core1 {
+    public static void main(String[] args) {
+ 
+        int num1 = 0b101;
+        System.out.println("num1 : " + num1);
+
+        // Hexa Decimal Number
+        int num2 = 0x7E;
+        System.out.println("num2 : " + num2);
+
+        int num3 = 10_00_00_00;
+        System.out.println("num2 : " + num3);
+
+        char a = 'c';
+        System.out.println("a : " + a);
+
+        a++;
+        System.out.println("a++ : " + a);
+    }
+}
